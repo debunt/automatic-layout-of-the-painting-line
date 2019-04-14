9b4b953a80e2f0e2dbd7d58de154b2b80bc98fb6
debunt <dsopichev@mail.ru>
2019-04-14 03:10:52 +0300

    Added GUI entities. Obstacles are not parsed yet. Grid drawed on canvas in order to represent set of blocks and routing in blocks

---
+++ envatcd/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\yandexdisk\#skoltech\early research tecnomax\pythonscript\envatcd\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
